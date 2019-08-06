b08649d34672ec3c6697561481202d9a76f2f185
student <student@ELC-MAC-AIR-18.local>
2019-08-05 21:16:39 -0500

    Added comments to the code

--- taco_model.py
+++ taco_model.py
@@ -1,5 +1,7 @@
+#this line is very important because it lets us use the ndb.Model in the class
 from google.appengine.ext import ndb
 
 
 class TacoFilling(ndb.Model):
+    # this is one property of type String
     tacoIn = ndb.StringProperty(required=True)
